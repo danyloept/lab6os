7925d46a78cc523c2e6f0c8d5bdc8d2466ee2b50
Daniil <bandiboy4@gmail.com>
2023-11-06 22:39:18 +0300

    calculate.c-changed

--- calculate.c
+++ calculate.c
@@ -42,7 +42,7 @@ float Calculate(float Numeral, char Operation[4]) {
     else if (strncmp(Operation, "tan", 3) == 0)
         return tan(Numeral);
     else {
-        printf("Неправильно введено действие! ");
+        printf("Wrong action");
         return HUGE_VAL;
     }
 }
